3937cb818facfdd699608cf5b649bded7a77168c
Matteo Conti <contimatteo1505@gmail.com>
2024-01-28 16:53:46 +0100

    WIP

--- run.sh
+++ run.sh
@@ -6,7 +6,14 @@ exit 0
 ### QUALITY EVALUATION
 ###
 
-CUDA_VISIBLE_DEVICES=2 python3 tt3d_eval_quality.py \
+CUDA_VISIBLE_DEVICES=0 python3 tt3d_eval_quality.py \
+  --model "fantasia3d" \
+  --prompt-file /media/data2/mconti/TT3D/prompts/test.v1.n2.txt \
+  --source-path /media/data2/mconti/TT3D/models/Threestudio-Fantasia3D/outputs \
+  --out-path /media/data2/mconti/TT3D/metrics/T3Bench/fantasia3d
+#  --skip-existing-renderings
+
+CUDA_VISIBLE_DEVICES=0 python3 tt3d_eval_alignment.py \
   --model "fantasia3d" \
   --prompt-file /media/data2/mconti/TT3D/prompts/test.v1.n2.txt \
   --source-path /media/data2/mconti/TT3D/models/Threestudio-Fantasia3D/outputs \
