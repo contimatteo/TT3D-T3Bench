70344ede9269abbe6d82f54520f2a442b94bd211
Matteo Conti <contimatteo1505@gmail.com>
2024-01-29 17:51:54 +0100

    WIP

--- tt3d_eval_alignment.py
+++ tt3d_eval_alignment.py
@@ -209,10 +209,12 @@ def _evaluate_alignment(model: str, prompt: str, out_rootpath: Path, merged_capt
     prompt_to_gpt4 = grounding
     prompt_to_gpt4 += '\n' + 'Prompt: ' + prompt + '\n'
     prompt_to_gpt4 += 'Prediction: ' + merged_caption
-    print(prompt_to_gpt4)
+    # print(prompt_to_gpt4)
     eval_result_text = _openai_gpt_eval_caption(prompt=prompt_to_gpt4, temperature=0)
 
+    print("")
     print(eval_result_text)
+    print("")
     # output += f'{np.round(float(res)):.0f}\t\t{prompt}\n'
 
     # print("Alignment Score:", mean_score)
