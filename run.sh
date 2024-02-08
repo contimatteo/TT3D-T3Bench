0346d051f9399f8fbd0e314c673e4ad76463640a
Matteo Conti <contimatteo1505@gmail.com>
2024-02-08 11:11:11 +0100

    WIP

--- run.sh
+++ run.sh
@@ -1,14 +1,13 @@
 ###
 
-# exit 0
+exit 0
 
 GPU=0
 PROMPT="test_t3bench_n1"
 
 ROOT_DIR="/media/data2/mconti/TT3D"
-PROMPT_DIR="${ROOT_DIR}/prompts"
 SOURCE_DIR="${ROOT_DIR}/outputs/${PROMPT}"
-PROMPT_FILE="${PROMPT_DIR}/${PROMPT}.txt"
+PROMPT_FILE="${ROOT_DIR}/prompts/${PROMPT}.txt"
 OUT_DIR="${ROOT_DIR}/metrics/T3Bench/${PROMPT}"
 
 
@@ -16,14 +15,7 @@ OUT_DIR="${ROOT_DIR}/metrics/T3Bench/${PROMPT}"
 ### QUALITY EVALUATION
 ###
 
-### Fantasia3D
-# CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_eval_quality.py \
-#   --model "fantasia3d" \
-#   --prompt-file $PROMPT_FILE \
-#   --source-path "${SOURCE_DIR}/Threestudio-Fantasia3D/" \
-#   --out-path "${OUT_DIR}"
-
-### ShapE
+### OpenAI-ShapE
 CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_eval_quality.py \
   --model "shap-e" \
   --prompt-file $PROMPT_FILE \
@@ -32,7 +24,7 @@ CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_eval_quality.py \
   --skip-existing-renderings \
   --skip-existing-scores
 
-### PointE
+### OpenAI-PointE
 CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_eval_quality.py \
   --model "point-e" \
   --prompt-file $PROMPT_FILE \
@@ -40,6 +32,61 @@ CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_eval_quality.py \
   --out-path "${OUT_DIR}" \
   --skip-existing-renderings \
   --skip-existing-scores
+
+### Threestudio-DreamFusion
+CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_eval_quality.py \
+  --model "dreamfusion-sd" \
+  --prompt-file $PROMPT_FILE \
+  --source-path "${SOURCE_DIR}/Threestudio-DreamFusion/" \
+  --out-path "${OUT_DIR}" \
+  --skip-existing-renderings \
+  --skip-existing-scores
+
+### Threestudio-Fantasia3D
+CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_eval_quality.py \
+  --model "fantasia3d" \
+  --prompt-file $PROMPT_FILE \
+  --source-path "${SOURCE_DIR}/Threestudio-Fantasia3D/" \
+  --out-path "${OUT_DIR}" \
+  --skip-existing-renderings \
+  --skip-existing-scores
+
+### Threestudio-ProlificDreamer
+CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_eval_quality.py \
+  --model "prolificdreamer" \
+  --prompt-file $PROMPT_FILE \
+  --source-path "${SOURCE_DIR}/Threestudio-ProlificDreamer/" \
+  --out-path "${OUT_DIR}" \
+  --skip-existing-renderings \
+  --skip-existing-scores
+
+### Threestudio-Magic3D
+CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_eval_quality.py \
+  --model "magic3d" \
+  --prompt-file $PROMPT_FILE \
+  --source-path "${SOURCE_DIR}/Threestudio-Magic3D/" \
+  --out-path "${OUT_DIR}" \
+  --skip-existing-renderings \
+  --skip-existing-scores
+
+### Threestudio-TextMesh
+CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_eval_quality.py \
+  --model "textmesh-sd" \
+  --prompt-file $PROMPT_FILE \
+  --source-path "${SOURCE_DIR}/Threestudio-TextMesh/" \
+  --out-path "${OUT_DIR}" \
+  --skip-existing-renderings \
+  --skip-existing-scores
+
+### Threestudio-HiFA
+CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_eval_quality.py \
+  --model "hifa" \
+  --prompt-file $PROMPT_FILE \
+  --source-path "${SOURCE_DIR}/Threestudio-HiFA/" \
+  --out-path "${OUT_DIR}" \
+  --skip-existing-renderings \
+  --skip-existing-scores
+
 
 ###
 ### ALIGNMENT EVALUATION
