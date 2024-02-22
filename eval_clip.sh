f5ffc15725839e1a5af94ec949cd8f6b9d84382e
Matteo Conti <contimatteo1505@gmail.com>
2024-02-22 18:24:43 +0100

    WIP

---
+++ eval_clip.sh
@@ -0,0 +1,36 @@
+###
+
+exit 1
+
+
+GPU=3
+ENV="report"
+PROMPT="n100"
+EXPERIMENT_PREFIX="t3bench/single"
+
+# ROOT_DIR="/media/data2/mconti/TT3D"
+ROOT_DIR="mconti/TT3D"
+SOURCE_DIR="/media/data2/${ROOT_DIR}/outputs/${ENV}/${EXPERIMENT_PREFIX}/${PROMPT}"
+OUT_DIR="/media/data2/${ROOT_DIR}/metrics/${ENV}/${EXPERIMENT_PREFIX}/${PROMPT}"
+PROMPT_FILE="/media/data2/${ROOT_DIR}/prompts/${EXPERIMENT_PREFIX}/${PROMPT}.txt"
+
+export TRANSFORMERS_OFFLINE=1
+export DIFFUSERS_OFFLINE=1
+export HF_DATASETS_OFFLINE=1
+export HF_HUB_OFFLINE=1
+
+
+###
+
+
+echo ">"
+echo "> [quality] ShapE"
+echo ">"
+
+### OpenAI-ShapE
+CUDA_VISIBLE_DEVICES=${GPU} python3 tt3d_eval_clip.py \
+  --model "shap-e" \
+  --prompt-file $PROMPT_FILE \
+  --source-path "${SOURCE_DIR}/OpenAI-ShapE/" \
+  --out-path "${OUT_DIR}" \
+  --skip-existing
